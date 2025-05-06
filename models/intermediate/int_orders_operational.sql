a4f110b27c8606c45551f0e7d11dbb1e609e979a
James Whittle <jameswhittle2012@gmail.com>
2025-05-06 19:37:21 +0000

    Add other columns for finance_days model and build finance_day query

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -19,6 +19,7 @@ SELECT
     orders_id,
     date_date,
     ROUND(margin+shipping_fee-logcost-ship_cost,2) AS operational_margin,
+    margin,
     quantity,
     revenue,
     purchase_cost,
---
+++ models/mart/finance_days.sql
@@ -0,0 +1,27 @@
+WITH daily_agg AS(
+SELECT
+    date_date,
+    COUNT(orders_id) AS nb_transactions,
+    SUM(revenue) AS revenue,
+    SUM(margin) AS margin,
+    SUM(operational_margin) AS operational_margin,
+    SUM(purchase_cost) AS purchase_cost,
+    SUM(shipping_fee) AS shipping_fee,
+    SUM(logcost) AS logcost,
+    SUM(quantity) AS quantity
+FROM {{ ref('int_orders_operational') }}
+GROUP BY date_date
+ORDER BY date_date desc)
+
+SELECT
+date_date,
+nb_transactions,
+ROUND(revenue,2) AS revenue,
+ROUND(revenue/nb_transactions,2) AS average_basket,
+ROUND(margin,2) AS margin,
+ROUND(operational_margin,2) AS operational_margin,
+ROUND(purchase_cost,2) AS purchase_cost,
+ROUND(shipping_fee,2) AS shipping_fee,
+ROUND(logcost,2) AS logcost,
+quantity
+FROM daily_agg
